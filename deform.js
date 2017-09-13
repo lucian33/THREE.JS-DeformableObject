6b3bdb25f78cb52201d519b320cf00066e3dda4f
lucian33 <lucian19602@gmail.com>
2017-09-13 12:30:54 -0500

    Add files
    migrate from codepen to github~

---
+++ deform.js
@@ -0,0 +1,129 @@
+/**
+ * A simple Galaxy
+ * @author haoz2@illinois.edu
+ */
+
+
+/**
+ * Initialize camera, scene and renderer
+ */
+let scene, camera, renderer;
+
+let width = window.innerWidth, height = window.innerHeight;
+
+let group, particles;
+
+initialize();
+
+render();
+
+function initialize(){
+
+  scene = new THREE.Scene();
+  //scene.background = new THREE.Color( 0x000000, 0 );
+  camera = new THREE.PerspectiveCamera(90, width / height, 1, 20);
+  camera.lookAt(scene.position);
+  camera.position.z = 5;
+
+  renderer = new THREE.WebGLRenderer({alpha:true});
+  renderer.setSize(width, height);
+  //renderer.setClearColor(0x0E2255);
+
+  document.getElementById('canvas').appendChild(renderer.domElement);
+
+  window.addEventListener('resize', onResize);
+
+
+  drawObjects();
+
+  createLight();
+}
+
+
+
+function onResize() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
+}
+
+/**
+ * createLight - crete light in the scene
+ */
+
+function createLight(){
+  var ambientLight = new THREE.AmbientLight(0x404040); // ambient light
+  scene.add(ambientLight);
+
+  var light = new THREE.DirectionalLight();
+  light.position.set(1, 2, 2);
+  scene.add(light);
+}
+
+/**
+ * drawObjects - Create planet and ring geometry
+ */
+
+function drawObjects(){
+  // collect all objects
+  group = [];
+  // basic geometry
+  var geometry = new THREE.IcosahedronGeometry(1,1);
+  // basic material
+  var blue = new THREE.MeshPhongMaterial({color:'#5b94ed', blending: THREE.NormalBlending, transparent: true, opacity: 0.9, shading: THREE.FlatShading});
+  var red = new THREE.MeshPhongMaterial({color:'#fb94ee', blending: THREE.NormalBlending, transparent: true, opacity: 0.9, shading: THREE.FlatShading});
+  var green = new THREE.MeshPhongMaterial({color:'#5bffcf', blending: THREE.NormalBlending, transparent: true, opacity: 0.9, shading: THREE.FlatShading});
+
+  var material = new THREE.MeshPhongMaterial({color:'#5b94ce', shading: THREE.FlatShading});
+
+  var obj1 = new THREE.Mesh(geometry, material);
+  var obj2 = new THREE.Mesh(geometry, material);
+  var obj3 = new THREE.Mesh(geometry, material);
+
+  // var obj1 = new THREE.Mesh(geometry, blue);
+  // var obj2 = new THREE.Mesh(geometry, red);
+  // var obj3 = new THREE.Mesh(geometry, green);
+
+  group.push(obj1);
+  group.push(obj2);
+  group.push(obj3);
+
+
+  scene.add(group[0], group[1], group[2]);
+}
+
+
+function render(){
+
+  requestAnimationFrame(render);
+
+  group[0].rotation.x += degToRad(0.1);
+  group[0].rotation.y += degToRad(0.1);
+  group[0].rotation.z += degToRad(0.3);
+
+  group[1].rotation.x += degToRad(-0.3);
+  group[1].rotation.y += degToRad(-0.1);
+  group[1].rotation.z += degToRad(-0.3);
+
+  group[2].rotation.x += degToRad(-0.1);
+  group[2].rotation.y += degToRad(0.1);
+  group[2].rotation.z += degToRad(-0.3);
+
+  scene.rotation.y += degToRad(0.5);
+
+  //planet.rotation.z += 0.03;
+  renderer.render(scene, camera);
+}
+
+
+/**
+ * degToRad - description
+ *
+ * @param  {type} deg degree
+ * @return {type} radius return the radius
+ */
+function degToRad(deg){
+    return deg / 180 * Math.PI;
+}
