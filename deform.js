47840f89c04cb61ffefccf1104eed7229e216bd4
lucian33 <lucian19602@gmail.com>
2017-09-13 12:33:27 -0500

    Update

--- deform.js
+++ deform.js
@@ -1,5 +1,5 @@
 /**
- * A simple Galaxy
+ * Deformable objects
  * @author haoz2@illinois.edu
  */
 
@@ -25,7 +25,7 @@ function initialize(){
   camera.lookAt(scene.position);
   camera.position.z = 5;
 
-  renderer = new THREE.WebGLRenderer({alpha:true});
+  renderer = new THREE.WebGLRenderer({alpha:true, antialias:true});
   renderer.setSize(width, height);
   //renderer.setClearColor(0x0E2255);
 
@@ -58,7 +58,7 @@ function createLight(){
   scene.add(ambientLight);
 
   var light = new THREE.DirectionalLight();
-  light.position.set(1, 2, 2);
+  light.position.set(1, 2, 1);
   scene.add(light);
 }
 
@@ -70,7 +70,7 @@ function drawObjects(){
   // collect all objects
   group = [];
   // basic geometry
-  var geometry = new THREE.IcosahedronGeometry(1,1);
+  var geometry = new THREE.IcosahedronGeometry(2,1);
   // basic material
   var blue = new THREE.MeshPhongMaterial({color:'#5b94ed', blending: THREE.NormalBlending, transparent: true, opacity: 0.9, shading: THREE.FlatShading});
   var red = new THREE.MeshPhongMaterial({color:'#fb94ee', blending: THREE.NormalBlending, transparent: true, opacity: 0.9, shading: THREE.FlatShading});
@@ -81,7 +81,7 @@ function drawObjects(){
   var obj1 = new THREE.Mesh(geometry, material);
   var obj2 = new THREE.Mesh(geometry, material);
   var obj3 = new THREE.Mesh(geometry, material);
-
+  var obj4 = new THREE.Mesh(geometry, material);
   // var obj1 = new THREE.Mesh(geometry, blue);
   // var obj2 = new THREE.Mesh(geometry, red);
   // var obj3 = new THREE.Mesh(geometry, green);
@@ -89,9 +89,9 @@ function drawObjects(){
   group.push(obj1);
   group.push(obj2);
   group.push(obj3);
+  group.push(obj4);
 
-
-  scene.add(group[0], group[1], group[2]);
+  scene.add(group[0], group[1], group[2], group[3]);
 }
 
 
@@ -101,17 +101,23 @@ function render(){
 
   group[0].rotation.x += degToRad(0.1);
   group[0].rotation.y += degToRad(0.1);
-  group[0].rotation.z += degToRad(0.3);
+  group[0].rotation.z += degToRad(0.1);
 
-  group[1].rotation.x += degToRad(-0.3);
+  group[1].rotation.x += degToRad(-0.1);
   group[1].rotation.y += degToRad(-0.1);
-  group[1].rotation.z += degToRad(-0.3);
+  group[1].rotation.z += degToRad(-0.1);
 
   group[2].rotation.x += degToRad(-0.1);
   group[2].rotation.y += degToRad(0.1);
-  group[2].rotation.z += degToRad(-0.3);
+  group[2].rotation.z += degToRad(-0.1);
+
+  group[3].rotation.x += degToRad(0.1);
+  group[3].rotation.y += degToRad(0.1);
+  group[3].rotation.z += degToRad(-0.1);
 
   scene.rotation.y += degToRad(0.5);
+  scene.rotation.x += degToRad(0.5);
+  scene.rotation.z += degToRad(0.5);
 
   //planet.rotation.z += 0.03;
   renderer.render(scene, camera);
